e3ecf57c1ca545e32f0ee97dd0e555715e3a2aa0
Alcides Turruellas <ayunierto@gmail.com>
2024-10-20 01:42:05 -0500

    creted custom pipe

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -81,8 +81,10 @@ export class PokemonService {
   }
 
   async remove(id: string) {
-    const pokemon = await this.findOne(id);
-    await pokemon.deleteOne();
-    return `This action removes a #${id} pokemon`;
+    // const pokemon = await this.findOne(id);
+
+    // await pokemon.deleteOne();
+    const result = this.pokemonModel.findByIdAndDelete(id);
+    return result;
   }
 }
