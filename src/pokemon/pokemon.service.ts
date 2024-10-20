6fc6c41c42f5eaf1c58a5aefa094225a8b2553ac
Alcides Turruellas <ayunierto@gmail.com>
2024-10-20 01:47:22 -0500

    validate delete pokemon

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -84,7 +84,11 @@ export class PokemonService {
     // const pokemon = await this.findOne(id);
 
     // await pokemon.deleteOne();
-    const result = this.pokemonModel.findByIdAndDelete(id);
-    return result;
+    // const result = await this.pokemonModel.findByIdAndDelete(id);
+    const { deletedCount } = await this.pokemonModel.deleteOne({ _id: id });
+    if (deletedCount === 0) {
+      throw new BadRequestException(`Pokemon with id "${id}" not found`);
+    }
+    return;
   }
 }
