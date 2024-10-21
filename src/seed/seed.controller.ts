4a5f08f78d87dc5d4d96f2234ecc9a8467b64848
Alcides Turruellas <ayunierto@gmail.com>
2024-10-20 21:19:40 -0500

    get pokemon name and no

---
+++ src/seed/seed.controller.ts
@@ -0,0 +1,21 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { SeedService } from './seed.service';
+import axios, { AxiosInstance } from 'axios';
+
+@Controller('seed')
+export class SeedController {
+  constructor(private readonly seedService: SeedService) {}
+
+  @Get()
+  executeSeed() {
+    return this.seedService.executeSeed();
+  }
+}
